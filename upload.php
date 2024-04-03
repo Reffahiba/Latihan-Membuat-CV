3846d0c0067e6be885375f29f491312943b5d3e5
Reffa Hibatullah <raffahibatullah888@gmail.com>
2024-04-03 07:46:20 +0700

    mengupdate agar bisa menambah gambar

--- upload.php
+++ upload.php
@@ -1,15 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Unggah Gambar</title>
-  </head>
-  <body>
-    <h2>Unggah Gambar</h2>
-    <form action="terima.php" method="post" enctype="multipart/form-data">
-      <input type="file" name="gambar" accept="image/*" />
-      <input type="submit" value="Unggah" />
-    </form>
-  </body>
-</html>
+<?php
+$file_name = $_FILES['gambar']['name'];
+$file_size = $_FILES['gambar']['size'];
+$file_tmp = $_FILES['gambar']['tmp_name'];
+$file_type = $_FILES['gambar']['type'];
+$target_file = $file_name;
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["gambar"]["tmp_name"]);
+  if($check !== false) {
+    echo "File ini adalah gambar - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File ini bukanlah gambar.";
+    $uploadOk = 0;
+  }
+}
+
+if ($uploadOk == 0) {
+  echo "Maaf, file belum terupload.";
+} else {
+  if (move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file)) {
+    echo "<img src='$target_file', alt='Gambar telah diupload'>";
+  } else {
+    echo "Maaf, terjadi error saat proses upload.";
+  }
+}
+?>
